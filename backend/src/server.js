c7072067aec6af4a40c981224fe81805e1a84e6f
z-Pearlina <pearlinaofficial@gmail.com>
2025-09-23 15:15:05 +0100

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,29 @@
-import express from 'express';
-import {ENV} from './config/env.js';
-import { connectDB } from './config/db.js';
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+import userRoutes from "./routes/user.route.js";
+
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from server...'));
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => res.send("Hello from server"));
+
+app.use("/api/users", userRoutes);
+
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
 
 const startServer = async () => {
   try {
