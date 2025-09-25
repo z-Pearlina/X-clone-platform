7b14b47bc1816cc88624fde292801a730132a650
z-Pearlina <pearlinaofficial@gmail.com>
2025-09-25 20:41:30 +0100

    rn-basic-setup

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+     <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
