53a28aa8a3f34508b48cb4d2e720600fa0b79d28
z-Pearlina <pearlinaofficial@gmail.com>
2025-09-25 15:02:10 +0100

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    // listen for local development
+    // listen for local dev
     if (ENV.NODE_ENV !== "production") {
       app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
     }
