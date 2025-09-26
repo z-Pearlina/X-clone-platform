86a5694be8f2ed23da5a78167ef5319bfeb30af2
z-Pearlina <pearlinaofficial@gmail.com>
2025-09-26 14:43:36 +0100

    auth implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,14 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
-import "../global.css"
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack/>;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(auth)/index" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native";
+import { View, Text , Button } from 'react-native'
+import React from 'react'
+import { useClerk } from '@clerk/clerk-expo'
 
-export default function Index() {
+const HomeScreen = () => {
+    const {signOut} = useClerk()
   return (
-     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View>
+      <Text>HomeScreen</Text>
+
+      <Button onPress={ () =>signOut()} title="logout"></Button>
     </View>
-  );
+  )
 }
+
+export default HomeScreen
