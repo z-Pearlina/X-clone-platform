c2025585ed2bbfb5e6bedc18e47fe69f4eb07a5d
z-Pearlina <pearlinaofficial@gmail.com>
2025-10-07 23:23:04 +0100

    comment functionality added

--- mobile/components/PostList.tsx
+++ mobile/components/PostList.tsx
@@ -4,6 +4,7 @@ import { Post } from "@/types";
 import { View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
 import PostCard from "./PostCard";
 import { useState } from "react";
+import CommentsModal from "./CommentsModal";
 
 const PostsList = ({ username }: { username?: string }) => {
   const { currentUser } = useCurrentUser();
@@ -55,6 +56,7 @@ const PostsList = ({ username }: { username?: string }) => {
         />
       ))}
 
+      <CommentsModal selectedPost={selectedPost} onClose={() => setSelectedPostId(null)} />
     </>
   );
 };
