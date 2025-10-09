4500f6b283478039952a1a149f146ebbdcc33da7
z-Pearlina <pearlinaofficial@gmail.com>
2025-10-09 21:14:21 +0100

    fix an unhandled crash

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,9 @@ import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use(clerkMiddleware());
 //app.use(arcjetMiddleware);  //Arcjet is causing a 403/500 error in Vercel
@@ -38,7 +40,9 @@ const startServer = async () => {
 
     // listen for local dev
     if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
     }
   } catch (error) {
     console.error("Failed to start server:", error.message);
