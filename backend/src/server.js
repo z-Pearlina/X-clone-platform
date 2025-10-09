69e27fc4c3fb1bed7f4a0793ce7369d67d76cf88
z-Pearlina <pearlinaofficial@gmail.com>
2025-10-09 22:55:18 +0100

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,12 +14,10 @@ import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 const app = express();
 
 app.use(cors());
-
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json());
 
 app.use(clerkMiddleware());
-//app.use(arcjetMiddleware);  //Arcjet is causing a 403/500 error in Vercel
+app.use(arcjetMiddleware);
 
 app.get("/", (req, res) => res.send("Hello from server"));
 
@@ -38,11 +36,9 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    // listen for local dev
+    // listen for local development
     if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () =>
-        console.log("Server is up and running on PORT:", ENV.PORT)
-      );
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
     }
   } catch (error) {
     console.error("Failed to start server:", error.message);
